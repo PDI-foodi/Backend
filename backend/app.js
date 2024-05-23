3a1154fa0f9312b14f4ee80455fb85913fad5a7f
HyeonJu LEE <140503027+Leehyeonju0219@users.noreply.github.com>
2024-05-23 16:35:13 +0900

    🐛 fix: index render->send로 수정(view engine 에러 해결)

--- backend/app.js
+++ backend/app.js
@@ -16,8 +16,8 @@ const reviewRouter = require("./routes/review");
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -35,15 +35,15 @@ app.use("/detail", detailRouter);
 app.use("/review", reviewRouter);
 
 //mongodb연결을위해 비밀번호 가림
-const dotenv=require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 //mongodb 연결
-const mongoose =require("mongoose");
-mongoose.connect(
-  process.env.MONGO_URI
-).then(()=>console.log("Connecting"))
-.catch(err=>console.log(err));
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connecting"))
+  .catch((err) => console.log(err));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
