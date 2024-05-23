c621cd8967212808a7039372d460296560f50ed7
HyeonJu LEE <140503027+Leehyeonju0219@users.noreply.github.com>
2024-05-23 20:22:35 +0900

    ✨ feat: 회원 로그인 구현
    로그인 성공 시, JWT token 발행

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+const { createToken } = require("../utils/auth");
 
 router.post("/signup", async (req, res, next) => {
   try {
     const { id, password, nickname } = req.body;
     const user = await User.signUp(id, password, nickname);
     res.status(201).json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ message: err.message });
+    next(err);
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const { id, password } = req.body;
+    const user = await User.login(id, password);
+    const tokenMaxAge = 60 * 60 * 24 * 3;
+    const token = createToken(user, tokenMaxAge);
+
+    user.token = token;
+
+    res.cookie("authToken", token, {
+      httpOnly: true,
+      maxAge: tokenMaxAge * 1000,
+    });
+
+    console.log(user);
+    res.status(200).json(user);
   } catch (err) {
     console.error(err);
     res.status(400).json({ message: err.message });
