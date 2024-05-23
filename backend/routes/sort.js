56e8610adf176c1d29b9e3473673b7b58491c07a
lemonticsoul <sjho714@naver.com>
2024-05-23 17:07:29 +0900

    ✨feat:카테고리 조회 기능 추가

--- backend/routes/sort.js
+++ backend/routes/sort.js
@@ -1,5 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const Restaurant=require('../models/Restaurant')
+
+
+router.get('/data',async (req,res)=>{
+    Restaurant.find().then((elem)=>{
+        elem.map((e)=>{
+            console.log(e.category);
+        })
+    })
+
+})
+
 
 module.exports = router;
