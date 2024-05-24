a4e8587e9533b0825f77e09fa682d56c88300c26
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-05-24 16:13:16 +0900

    ✨  feat : 상세페이지 찜 버튼 누를시 like테이블에 목록 저장 완료

--- backend/routes/detail.js
+++ backend/routes/detail.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const Restaurant = require("../models/Restaurant");
+const LikeSchema = require("../models/Like");
 
 /* router.get("/:id", (req, res) => {
   console.log(req.data);
@@ -14,6 +15,16 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   Restaurant.findById(req.params.id).then((result) => {
     res.json(result);
+  });
+});
+
+router.post("/:id", (req, res) => {
+  const { userId } = req.body;
+  LikeSchema.create({
+    userId: userId,
+    restaurantId: req.params.id,
+  }).then((result) => {
+    res.status(200).json(result);
   });
 });
 
