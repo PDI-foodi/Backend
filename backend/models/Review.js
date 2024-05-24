d7d4dfbc62d3fc981ee644c6e8a8abee2c142f86
HyeonJu LEE <140503027+Leehyeonju0219@users.noreply.github.com>
2024-05-24 15:25:43 +0900

    🐛 fix: Auto-increment id 삭제(mongoose에서 자동으로 _id 생성)

--- backend/models/Review.js
+++ backend/models/Review.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const reviewSchema = new mongoose.Schema(
   {
-    id: { type: Number, required: true },
     userId: { type: Number, required: true },
     content: { type: String, required: true },
     createdAt: { type: Date, required: true },
