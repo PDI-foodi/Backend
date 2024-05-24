493d78fb8070d52c1ae0c3b40ff41f3be07ef1af
HyeonJu LEE <140503027+Leehyeonju0219@users.noreply.github.com>
2024-05-24 16:34:39 +0900

    🐛 fix: restaurantId 추가해서 review 등록. next 추가하여 err 전달

--- backend/routes/review.js
+++ backend/routes/review.js
@@ -2,9 +2,10 @@ var express = require("express");
 var router = express.Router();
 
 const Review = require("../models/Review");
+const Restaurant = require("../models/Restaurant");
 const User = require("../models/User");
 
-router.post("/", async (req, res) => {
+router.post("/", async (req, res, next) => {
   try {
     const { userId, restaurantId, content } = req.body;
 
@@ -18,11 +19,12 @@ router.post("/", async (req, res) => {
       return res.status(404).json({ message: "Restaurant not found" });
     }
 
-    const review = await Review.create({ userId, content });
+    const review = await Review.create({ userId, restaurantId, content });
     res.status(201).json(review);
   } catch (err) {
     console.error(err);
     res.status(400).json({ message: err.message });
+    next(err);
   }
 });
 
