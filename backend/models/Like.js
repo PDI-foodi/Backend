52f8d5d073bdf790933bcd1c08db412bf84e58a0
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-05-24 16:14:01 +0900

    ♻ refactor : id값은 필요하지 않아서 제거

--- backend/models/Like.js
+++ backend/models/Like.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose");
 
 const likeSchema = new mongoose.Schema(
   {
-    id: { type: Number, required: true },
-    userId: { type: Number, required: true },
-    restaurantId: { type: Number, required: true },
+    userId: { type: String, required: true },
+    restaurantId: { type: String, required: true },
   },
   {
     timestamps: true,
