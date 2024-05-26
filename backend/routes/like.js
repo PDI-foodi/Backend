02b3e7e55b4e1e4d8a032171e972b1d8ec7180e4
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-05-26 21:59:22 +0900

    ✨ feat : 찜 버튼 눌렀을 때 로그인된 id에 해당하는 찜 가게 불러오기

--- backend/routes/like.js
+++ backend/routes/like.js
@@ -1,5 +1,42 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const Like = require("../models/Like");
+const Restaurant = require("../models/Restaurant");
+const User = require("../models/User");
+const { verifyToken } = require("../utils/auth");
+
+router.get("/", async (req, res) => {
+  try {
+    const token = req.cookies.authToken;
+
+    if (!token) {
+      return res.status(401).json({ message: "No token" });
+    }
+    const decoded = verifyToken(token);
+
+    if (!decoded) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+
+    const userId = decoded.id;
+    const likes = await Like.find({ userId });
+    const restaurantIds = likes.map((like) => like.restaurantId);
+    const restaurants = await Restaurant.find({ _id: { $in: restaurantIds } });
+
+    res.json(restaurants);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/user", (req, res) => {
+  User.find().then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
 
 
 module.exports = router;
