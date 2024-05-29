c7e0f628f18384e408eb8a317745dbcff957403d
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-05-29 17:40:23 +0900

    ✨ feat : 유저 아이디로 고유 userId 조회하는 기능 추가

--- backend/routes/nickname.js
+++ backend/routes/nickname.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const { verifyToken } = require("../utils/auth");
-
+const User = require("../models/User");
 
 router.get("/", async (req, res) => {
   try {
@@ -9,6 +9,18 @@ router.get("/", async (req, res) => {
     const decoded = verifyToken(token);
     const userNickname = decoded.nickname;
     res.json(userNickname);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+router.get("/id", async (req, res) => {
+  try {
+    const token = req.cookies.authToken;
+    const decoded = verifyToken(token);
+    const userNick = decoded.id;
+    User.find({ id: userNick }).then((result) => {
+      res.json(result[0]._id);
+    });
   } catch (error) {
     res.status(500).json(error);
   }
