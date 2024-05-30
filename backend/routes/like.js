5f9559b2a4d04074a1f3e29aa6099e843a3fa343
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-05-30 13:02:55 +0900

    ✨feat: 찜 삭제 기능 추가

--- backend/routes/like.js
+++ backend/routes/like.js
@@ -5,7 +5,6 @@ const Restaurant = require("../models/Restaurant");
 const User = require("../models/User");
 const { verifyToken } = require("../utils/auth");
 
-
 router.get("/", async (req, res) => {
   try {
     const token = req.cookies.authToken;
@@ -16,9 +15,51 @@ router.get("/", async (req, res) => {
     const restaurants = await Restaurant.find({ _id: { $in: restaurantIds } });
 
     res.json(restaurants);
-
   } catch (error) {
     res.status(500).json(error);
+  }
+});
+
+router.delete("/", async (req, res) => {
+  try {
+    const { userId, restaurantId } = req.body;
+    const result = await Like.findOneAndDelete({
+      userId: userId,
+      restaurantId: restaurantId,
+    });
+
+    if (result) {
+      res.status(200).json({ success: true, message: "삭제되었습니다." });
+    } else {
+      res
+        .status(404)
+        .json({ success: false, message: "해당 항목을 찾을 수 없습니다." });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: "서버 오류입니다.", error });
+  }
+});
+
+router.get("/findjjim", async (req, res) => {
+  const { userId, restaurantId } = req.query;
+
+  try {
+    const like = await Like.findOne({
+      userId: userId,
+      restaurantId: restaurantId,
+    });
+
+    if (like) {
+      res.status(200).json();
+    } else {
+      res.status(201).json();
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: "서버 오류입니다.", error });
   }
 });
 
